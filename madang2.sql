d5801979deb6f5c8def02f9f5bd491b15749a061
Kangjik Kim <rkqqkb@gmail.com>
2023-03-23 16:18:37 +0900

    exercise 2

---
+++ madang2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Imported_Book (
+  bookid      NUMBER ,
+  bookname    VARCHAR(40),
+  publisher   VARCHAR(40),
+  price       NUMBER(8) 
+);
+INSERT INTO Imported_Book VALUES(21, 'Zen Golf', 'Pearson', 12000);
+INSERT INTO Imported_Book VALUES(22, 'Soccer Skills', 'Human Kinetics', 15000);
+
+COMMIT;
